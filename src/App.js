07fb2b54ce1ea149e8784378d3ae3f9cd02c4cec
ehdrjs4502 <ehdrjs4502@naver.com>
2024-07-14 20:01:30 +0900

    react3fiber 공부

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useRef, useState, useEffect } from "react";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Text, useGLTF } from "@react-three/drei";
 
 function Shiba() {
-  const gltf = useLoader(GLTFLoader, "/shiba/scene.gltf");
+  const gltf = useGLTF("/shiba/scene.gltf");
   const ref = useRef();
   const [isTumbling, setIsTumbling] = useState(false);
   const [rotationX, setRotationX] = useState(0);
 
   useEffect(() => {
     if (isTumbling) {
+      console.log(ref.current);
       const targetRotation = ref.current.rotation.y + Math.PI * 2;
       const startTime = Date.now();
 
@@ -41,9 +41,31 @@ function Shiba() {
   return <primitive ref={ref} object={gltf.scene} onClick={handleClick} />;
 }
 
+function Box(props) {
+  const ref = useRef();
+  const [color, setColor] = useState(0);
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+    const c = (Math.sin(color) + 1) / 2;
+    ref.current.material.color.setRGB(c, 0, 1 - c);
+    setColor(color + 0.01);
+  });
+
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial />
+    </mesh>
+  );
+}
+
 function App() {
   return (
-    <Canvas camera={{ fov: 75, position: [0.1, 0.3, 1] }}>
+    <Canvas camera={{ position: [0, 0, 2.5], fov: 75 }}>
+      <Text position={[0, 1, 0]} fontSize={0.4} fontWeight={700} color="black">
+        시바견
+      </Text>
       <ambientLight />
       <Shiba />
       <OrbitControls />
